91eef91224325f0286ee55d0f8b6fc4a6d4f353b
DqrkEvil <DqrkEvil11@gmail.com>
2024-05-02 13:30:08 +0200

    Add randomised protection order and output file dumps

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import src.obfuscation.encode as encode
 import src.obfuscation.compress as cmp
 
 import sys
+import random
 
 def main():
     # Settings
@@ -13,15 +14,27 @@ def main():
     
     fileContents = fileContents.decode("utf-8")
     
-    # Applying protection methods
-    fileContents = encode.b64Encoding(fileContents)
+    # Randomising and applying protection methods
+    protectionMethods = [
+        encode.b64Encoding,
+        cmp.lzmaCompress,
+        cmp.zlibCompress
+    ] 
     
-    fileContents = cmp.lzmaCompress(fileContents)
+    random.shuffle(protectionMethods)
+    
+    for n, method in enumerate(protectionMethods):
+        fileContents = method(fileContents)
+        
+            # Creating new file with protections applied
+        with open(f"tempFile{n}.py", "w") as protectedFile:
+            protectedFile.write(fileContents)
+            protectedFile.truncate()
         
         # Creating new file with protections applied
     with open("protectedFile.py", "w") as protectedFile:
         protectedFile.write(fileContents)
-        protectedFile.truncate()
-        
+        protectedFile.truncate()        
+            
 if __name__ == "__main__":
     main()
